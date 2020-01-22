4dcbc7a1bb5a0b6ccd6409f066f53b11ed57aee0
Dani Tox <danitoxnu@gmail.com>
2020-01-22 08:14:48 +0100

    continue adding SwiftUI

---
+++ Pneumatica-SpriteKit-iOS/GenericAgent.swift
@@ -0,0 +1,106 @@
+//
+//  GenericAgent.swift
+//  Pneumatica-SpriteKit-iOS
+//
+//  Created by Daniel Fortesque on 21/01/2020.
+//  Copyright © 2020 Dani Tox. All rights reserved.
+//
+
+import Foundation
+import DXPneumatic
+
+class GenericAgent: ObservableObject {
+    @Published var isShowingValvoleList: Bool = false
+    
+    var valvolaCreationCompletion: ((UIValvola) -> Void)? = nil
+    
+    public func createInstanceOf(type: ValvoleTypes) {
+        switch type {
+        case .and:
+            let object = IPAnd(name: "AND")
+            object.postInit()
+            
+            let node = DXPneumatic.ValvolaAnd(model: object, size: .init(width: 100, height: 50))
+            valvolaCreationCompletion?(node)
+        case .or:
+            let object = IPOr(name: "OR")
+            object.postInit()
+            
+            let node = DXPneumatic.ValvolaOR(model: object, size: .init(width: 100, height: 50))
+            valvolaCreationCompletion?(node)
+        case .frl:
+            let object = IPFrl(name: "FRL")
+            object.postInit()
+            
+            let node = DXPneumatic.GruppoFRL(model: object, size: .init(width: 100, height: 50))
+            valvolaCreationCompletion?(node)
+        case .cde:
+            let object = IPCilindroDE(name: "CDE")
+            object.postInit()
+            
+            let node = DXPneumatic.CilindroDoppioEffetto(model: object, size: .init(width: 200, height: 50))
+            valvolaCreationCompletion?(node)
+        case .treDueMS:
+            let object = IPTreDueMS(name: "3/2 MS")
+            object.postInit()
+            
+            let node = DXPneumatic.TreDueMonostabileNC(model: object, size: .init(width: 100, height: 50))
+            valvolaCreationCompletion?(node)
+        case .treDueBS:
+            let object = IPTreDueBS(name: "3/2 BS")
+            object.postInit()
+            
+            let node = DXPneumatic.TreDueBistabile(model: object, size: .init(width: 100, height: 50))
+            valvolaCreationCompletion?(node)
+        case .timer:
+            let object = IPTimer(name: "Timer")
+            object.postInit()
+            
+            let node = DXPneumatic.SpriteTimer(model: object, size: .init(width: 100, height: 50))
+            valvolaCreationCompletion?(node)
+        case .cinqueDueMS:
+            let object = IPCinqueDueMS(name: "5/2 MS")
+            object.postInit()
+            
+            let node = DXPneumatic.CinqueDueMonostabile(model: object, size: .init(width: 100, height: 50))
+            valvolaCreationCompletion?(node)
+        case .cinqueDueBS:
+            let object = IPCinqueDueBS(name: "5/2 BS")
+            object.postInit()
+            
+            let node = DXPneumatic.CinqueDueBistabile(model: object, size: .init(width: 100, height: 50))
+            valvolaCreationCompletion?(node)
+        case .pulsante:
+            let object = IPPulsante(name: "Pulsante")
+            object.postInit()
+            
+            let node = DXPneumatic.Pulsante(model: object, size: .init(width: 100, height: 50))
+            valvolaCreationCompletion?(node)
+        case .finecorsa:
+            let object = IPFinecorsa(name: "Finecorsa")
+            object.postInit()
+            
+            let node = DXPneumatic.Finecorsa(model: object, size: .init(width: 100, height: 50))
+            valvolaCreationCompletion?(node)
+        }
+    }
+    
+}
+
+enum ValvoleTypes: CaseIterable {
+    case and
+    case or
+    case frl
+    case cde
+    case treDueMS
+    case treDueBS
+    case timer
+    case cinqueDueMS
+    case cinqueDueBS
+    case pulsante
+    case finecorsa
+    
+    static func type(at index: Int) -> ValvoleTypes {
+        return ValvoleTypes.allCases[index]
+    }
+}
