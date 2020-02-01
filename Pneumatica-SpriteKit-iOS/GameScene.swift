8633aad2312db1b27e1c4031e5d03093846a2db6
DaniTox <danitox@protonmail.com>
2020-02-01 21:50:46 +0100

    changed movable and observing implementations using Int instead of Double so i can work better with them and this has let me fixed the bug with finecorsa. Now have to add some tolerance in the check of finecorsa

--- Pneumatica-SpriteKit-iOS/GameScene.swift
+++ Pneumatica-SpriteKit-iOS/GameScene.swift
@@ -19,8 +19,6 @@ class GameScene: SKScene {
     
     var defaultBackground: UIColor!
     
-//    var firstSelectedIO: InputOutput?
-//    var secondSelectedIO: InputOutput?
     var lines: [Line] = []
     
     var holdRecognizer: UILongPressGestureRecognizer!
@@ -33,13 +31,6 @@ class GameScene: SKScene {
     var newValvoleNodes: [ValvolaStateReceiver] = []
     
     let cameraNode = SKCameraNode()
-    
-//    var newSelectedValvola: UIValvola?
-//    var newFirstSelectedIO: GraphicalIO?
-//    var newSecondSelectedIO: GraphicalIO?
-    
-    
-//    var isDragging: Bool = false
     
     //MARK: - Lifecycle
     override func didMove(to view: SKView) {
@@ -371,38 +362,4 @@ class GameScene: SKScene {
         }
     }
     
-}
-
-extension GameScene : UITableViewDelegate {
-//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//        hideTableView()
-//        guard let dataSource = self.dataSource as? ObjectCreationDataSource else { return }
-//
-//        let node = dataSource.createInstanceOf(type: ValvoleTypes.type(at: indexPath.row))
-//
-//        self.newValvoleNodes.append(node)
-//        self.runtime.addInCircuit(valvola: node.valvolaModel, hasToInitialize: false)
-//        if let startingPoint = node as? DXPneumatic.GruppoFRL {
-//            self.runtime.addStartingPoint(startingPoint.valvolaModel)
-//        }
-//
-//        if let observable = node as? DXPneumatic.ChangeListener {
-//            let listener = Listener(uiObject: observable, model: node.valvolaModel)
-//            self.runtime.observables.append(listener)
-//        }
-//
-//        if let finecorsa = node as? DXPneumatic.Finecorsa {
-//            guard let object = self.runtime.valvole.first(where: { $0 is IPCilindroDE }) else { fatalError() }
-//            guard let cilindro = object as? IPCilindroDE else { fatalError() }
-//            guard let finecorsaModel = finecorsa.valvolaModel as? IPFinecorsa else { fatalError() }
-//            runtime.observe(object: cilindro, observingObject: finecorsaModel)
-//        }
-//
-//        present(valvola: node)
-//
-//    }
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 80
-    }
 }
